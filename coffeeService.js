ef4b7ff2631fe8dfa6acd4fb696493ab60dbb60b
EugenePrygaro <eugeneprygaro@gmail.com>
2026-03-20 21:16:21 +0200

    refactor core logic

--- coffeeService.js
+++ coffeeService.js
@@ -1,3 +1,6 @@
 const prices = { espresso: 30, latte: 50, cappuccino: 45 };
-const getPrice = (type) => prices[type] || 0;
+const getPrice = (type) => {
+  const price = prices[type.toLowerCase()];
+  return price ? `${price} UAH` : "Not available";
+};
 module.exports = { getPrice };
