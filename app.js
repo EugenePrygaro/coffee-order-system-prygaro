0fe5876dbe58971a1f2e2af58b0dbcf7b6d32565
EugenePrygaro <eugeneprygaro@gmail.com>
2026-03-22 15:51:35 +0200

    delete logs

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const { getPrice } = require("./coffeeService");
 const { isValidType } = require("./validator");
 const type = "Mocha";
 
-console.log("Order started"); // додане логування
 if (isValidType(type)) {
   console.log(`Price: ${getPrice(type)}`);
 } else {
   console.log("Error: We don't have this coffee.");
 }
-console.log("Order finish");
